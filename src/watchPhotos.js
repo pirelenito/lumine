9c456b595e8e0f60f3013844273a61484ed3a831
Paulo Ragonha <paulo@ragonha.me>
2017-11-18 22:05:44 +0100

    Watch everything in masters 🌍

--- src/watchPhotos.js
+++ src/watchPhotos.js
@@ -5,13 +5,15 @@ const crypto = require('crypto')
 
 module.exports = basePath => {
   return create((add, end, error) => {
-    const pattern = path.join(basePath, '/**/*.@(ARW|jpg|jpeg|avi|mpeg)')
+    const pattern = path.join(basePath, '/**/*')
     const watcher = chokidar.watch(pattern)
 
     const publish = (filePath, stats) => {
       const relativePath = path.relative(basePath, filePath)
       const hash = crypto.createHash('sha256')
       hash.update(`${relativePath}${stats.mtimeMs}${stats.ctimeMs}`)
+
+      if (!filePath.match(/\.(arw|jpg|jpeg|)$/i)) return
 
       add({
         id: hash.digest('hex'),
