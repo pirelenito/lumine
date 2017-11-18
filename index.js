a7ab2a63954839e41e011f4b75748dd708c58e56
Paulo Ragonha <paulo@ragonha.me>
2017-11-18 22:13:47 +0100

    Making room for the client code 🏡

--- index.js
+++
@@ -1,7 +0,0 @@
-const gm = require('gm').subClass({ imageMagick: true })
-const path = require('path')
-
-gm(path.join(__dirname, 'pics/DSC02482.ARW')).size(function(err, value) {
-  if (err) console.log(err)
-  console.log('size', value)
-})
