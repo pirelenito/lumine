683a30b0d2012ce6689f65d6248a69b557529936
Paulo Ragonha <paulo@ragonha.me>
2018-11-25 21:29:33 +0100

    Adds more examples to check if the data photo import is working 👻

--- server/src/importPhoto/index.spec.ts
+++ server/src/importPhoto/index.spec.ts
@@ -1,24 +1,20 @@
-import loadPhoto from '.'
 import { join } from 'path'
+import { readdirSync } from 'fs'
 import rimraf from 'rimraf'
+
+import loadPhoto from '.'
 
 const libraryBasePath = join(__dirname, '../../fixtures')
 const cacheBasePath = join(__dirname, '../../tmp')
 const config = { libraryBasePath, cacheBasePath }
 
-beforeEach(() => rimraf.sync(cacheBasePath))
+beforeAll(() => rimraf.sync(cacheBasePath))
 
-it('loads a6000', async () => {
-  const photo = await loadPhoto(config)('a6000.ARW')
-  expect(photo).toMatchSnapshot()
-}, 180000)
-
-it('loads iphone 6', async () => {
-  const photo = await loadPhoto(config)('iphone6.jpg')
-  expect(photo).toMatchSnapshot()
-}, 180000)
-
-it('loads nokia 5', async () => {
-  const photo = await loadPhoto(config)('nokia5.jpg')
-  expect(photo).toMatchSnapshot()
-}, 180000)
+readdirSync(libraryBasePath)
+  .filter(photoPath => photoPath.match(/\.(arw|jpg|jpeg|)$/i))
+  .forEach((photoPath: string) => {
+    it(`loads ${photoPath}`, async () => {
+      const photo = await loadPhoto(config)(photoPath)
+      expect(photo).toMatchSnapshot()
+    }, 180000)
+  })
