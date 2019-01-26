516df1212b0629fd01ac63d550dd9c444119826f
Paulo Ragonha <paulo@ragonha.me>
2019-01-26 02:59:17 +0100

    Add a /api route to all server routes

--- server/src/startHttpServer.ts
+++ server/src/startHttpServer.ts
@@ -7,16 +7,16 @@ import { getThumbnail, getFullSize } from './previews'
 export default (config: Config) => (library: Library) => {
   const app = express()
 
-  app.get('/', (req, res) => res.json(library.getPhotos()))
+  app.get('/api/photos', (req, res) => res.json(library.getPhotos()))
 
-  app.get('/thumbnail/:contentHash', async (req, res) => {
+  app.get('/api/thumbnail/:contentHash', async (req, res) => {
     const photo = library.getPhotoByContentHash(req.params.contentHash)
     if (!photo) res.sendStatus(404)
 
     res.sendFile(await getThumbnail(config)(photo.contentHash, photo.relativePath))
   })
 
-  app.get('/fullSize/:contentHash', async (req, res) => {
+  app.get('/api/fullSize/:contentHash', async (req, res) => {
     const photo = library.getPhotoByContentHash(req.params.contentHash)
     if (!photo) res.sendStatus(404)
 
