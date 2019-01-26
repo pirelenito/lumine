99d01336d4a0f3b08d35171b3d2119c409f86674
Paulo Ragonha <paulo@ragonha.me>
2019-01-26 02:58:46 +0100

    Simple and somewhat broken Galery for all the pictures

---
+++ client/src/Galery.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import { Grid } from 'react-virtualized'
+
+interface Photo {
+  relativePath: string
+  contentHash: string
+  metadata: Metadata
+}
+
+interface Metadata {
+  createdAt: number
+  cameraModel?: string | number
+  gps?: GPS
+}
+
+interface GPS {
+  altitude: number
+  latitude: number
+  longitude: number
+}
+
+export default function Galery({}) {
+  const [photos, setPhotos] = useState<Photo[]>([])
+  const [height, setHeight] = useState(window.innerHeight)
+  const [width, setWidth] = useState(window.innerWidth)
+  const columnCount = Math.floor(width / 200)
+  const rowCount = Math.floor(photos.length / columnCount)
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setHeight(window.innerHeight)
+      setWidth(window.innerWidth)
+    })
+
+    fetch('/api/photos')
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(photos) {
+        setPhotos(photos)
+      })
+  }, [])
+
+  return (
+    <Grid
+      cellRenderer={({ columnIndex, key, rowIndex, style }: any) => {
+        const thumbnail = `/api/thumbnail/${photos[rowIndex * 4 + columnIndex].contentHash}`
+
+        return (
+          <div key={key} style={style}>
+            <img src={thumbnail} style={{ padding: 5, width: 190, height: 190 }} />
+          </div>
+        )
+      }}
+      columnCount={columnCount}
+      rowCount={rowCount}
+      height={height}
+      width={width}
+      columnWidth={200}
+      rowHeight={200}
+    />
+  )
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Galery from './Galery'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Galery />, document.getElementById('root'))
