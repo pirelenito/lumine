43185a6f78ccb22a184f5ac126f3c843fda652f8
Paulo Ragonha <paulo@ragonha.me>
2019-08-06 21:17:50 +0200

    Change hash function to be faster

---
+++ server/src/importPhoto/calculateContentHash.spec.ts
@@ -0,0 +1,8 @@
+import path from 'path'
+import calculateContentHash from './calculateContentHash'
+
+it('calculates the hash', async () => {
+  const fixturePath = path.join(__dirname, '../../fixtures/nokia5.jpg')
+  const hash = await calculateContentHash(fixturePath)
+  expect(hash).toEqual('22b05500001674cc286d20000')
+})
--- server/src/importPhoto/calculateContentHash.ts
+++ server/src/importPhoto/calculateContentHash.ts
@@ -1,9 +1,18 @@
 import { promisify } from 'util'
-import { exec } from 'child_process'
+import fs from 'fs'
+import pad from 'pad'
 
-const promisifiedExec = promisify(exec)
+const promisifiedStats = promisify(fs.stat)
 
+/**
+ * This creates a quick "hash" based on modification time and size
+ * Based on http://manpages.ubuntu.com/manpages/xenial/en/man1/rsync.1.html
+ */
 export default async function calculateHash(fullPath: string) {
-  const output = await promisifiedExec(`shasum -a 1 "${fullPath}"`)
-  return output.stdout.match(/^(\S+)\s+/)[1]
+  const stats = await promisifiedStats(fullPath)
+  const size = stats.size
+  const modificationTime = stats.mtime.getTime()
+
+  const output = `${pad(size.toString(16), 10, '0')}${pad(modificationTime.toString(16), 15, '0')}`
+  return output
 }
