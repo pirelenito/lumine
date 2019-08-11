8420dc78032ebacf077b2def977af20a48568c79
Paulo Ragonha <paulo@ragonha.me>
2019-08-11 14:06:29 +0200

    ⌨️ Keyboard navigation

--- client/src/routes/Galery.tsx
+++ client/src/routes/Galery.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Grid } from 'react-virtualized'
+import { Grid, ArrowKeyStepper } from 'react-virtualized'
 import { useGalery } from '../galery'
 import { Link } from 'react-router-dom'
 
@@ -17,39 +17,61 @@ export default function Galery() {
     })
   }, [])
 
-  return (
-    <Grid
-      cellRenderer={({ columnIndex, key, rowIndex, style }: any) => {
-        const photo = galery[rowIndex * columnCount + columnIndex]
-        const thumbnail = `/api/thumbnail/${photo.contentHash}`
+  const [scrollPosition, setScrollPosition] = useState({ scrollToColumn: 0, scrollToRow: 0 })
 
+  return (
+    <ArrowKeyStepper
+      isControlled
+      rowCount={rowCount}
+      mode="cells"
+      columnCount={columnCount}
+      onScrollToChange={setScrollPosition}
+      scrollToColumn={scrollPosition.scrollToColumn}
+      scrollToRow={scrollPosition.scrollToRow}
+    >
+      {({ onSectionRendered, scrollToColumn, scrollToRow }) => {
         return (
-          <div key={key} style={style}>
-            <Link to={`/media/${photo.mediaType}/${photo.contentHash}`}>
-              <img src={thumbnail} style={{ padding: 5, width: 190, height: 190 }} />
-              <div
-                style={{
-                  position: 'absolute',
-                  bottom: 5,
-                  left: 5,
-                  right: 5,
-                  textAlign: 'center',
-                  color: 'white',
-                  background: 'rgba(0,0,0,0.5)',
-                }}
-              >
-                {new Date(photo.metadata.createdAt).toLocaleDateString()}
-              </div>
-            </Link>
-          </div>
+          <Grid
+            cellRenderer={({ columnIndex, key, rowIndex, style }: any) => {
+              const photo = galery[rowIndex * columnCount + columnIndex]
+              const thumbnail = `/api/thumbnail/${photo.contentHash}`
+
+              return (
+                <div key={key} style={style}>
+                  <Link to={`/media/${photo.mediaType}/${photo.contentHash}`}>
+                    <img src={thumbnail} style={{ padding: 5, width: 190, height: 190 }} />
+                    <div
+                      style={{
+                        position: 'absolute',
+                        bottom: 5,
+                        left: 5,
+                        right: 5,
+                        textAlign: 'center',
+                        color: 'white',
+                        background:
+                          rowIndex === scrollToRow && columnIndex === scrollToColumn
+                            ? 'rgba(0,255,0,0.5)'
+                            : 'rgba(0,0,0,0.5)',
+                      }}
+                    >
+                      {new Date(photo.metadata.createdAt).toLocaleDateString()}
+                    </div>
+                  </Link>
+                </div>
+              )
+            }}
+            columnCount={columnCount}
+            rowCount={rowCount}
+            height={height}
+            width={width}
+            columnWidth={200}
+            rowHeight={200}
+            onSectionRendered={onSectionRendered}
+            scrollToColumn={scrollToColumn}
+            scrollToRow={scrollToRow}
+          />
         )
       }}
-      columnCount={columnCount}
-      rowCount={rowCount}
-      height={height}
-      width={width}
-      columnWidth={200}
-      rowHeight={200}
-    />
+    </ArrowKeyStepper>
   )
 }
