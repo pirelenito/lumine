94f67f76b222ace42abdc0d3c17b9d5424d971f7
Paulo Ragonha <paulo@ragonha.me>
2019-08-18 14:47:04 +0200

    🤸‍♀️ Limit parallel tasks

--- server/src/startHttpServer.ts
+++ server/src/startHttpServer.ts
@@ -1,10 +1,15 @@
+import os from 'os'
 import express from 'express'
-
+import { TaskQueue } from 'cwait'
 import Library from './Library'
 import Config from './Config'
 import { getThumbnail, getFullSize } from './previews'
 
 export default (config: Config) => (library: Library) => {
+  const queue = new TaskQueue(Promise, os.cpus().length - 1)
+  const getThumbnailThrottled = queue.wrap(getThumbnail(config))
+  const getFullSizeThrottled = queue.wrap(getFullSize(config))
+
   const app = express()
 
   app.get('/api/photos', (req, res) => res.json(library.getPhotos()))
@@ -16,7 +21,7 @@ export default (config: Config) => (library: Library) => {
     if (!photo) res.sendStatus(404)
 
     try {
-      res.sendFile(await getThumbnail(config)(photo.contentHash, photo.relativePath))
+      res.sendFile(await getThumbnailThrottled(photo.contentHash, photo.relativePath))
     } catch (error) {
       res.status(500)
       res.send(error.toString())
@@ -28,7 +33,7 @@ export default (config: Config) => (library: Library) => {
     if (!photo) res.sendStatus(404)
 
     try {
-      res.sendFile(await getFullSize(config)(photo.contentHash, photo.relativePath))
+      res.sendFile(await getFullSizeThrottled(photo.contentHash, photo.relativePath))
     } catch (error) {
       res.status(500)
       res.send(error)
