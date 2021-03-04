9e95b1663b612dd549ca23991798998244211bcf
Paulo Ragonha <paulo@ragonha.me>
2021-03-04 09:29:15 +0100

    Simple ESLint config to make VSCode happy 😄

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier/@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+}
