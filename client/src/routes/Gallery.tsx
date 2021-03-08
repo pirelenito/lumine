3669ac0c84cdbf97aed50a59f382e53bd6d9aafd
Paulo Ragonha <paulo@ragonha.me>
2021-03-08 15:33:05 +0100

    Tweak thumbnail size and spinner 🦄

--- client/src/routes/Gallery.tsx
+++ client/src/routes/Gallery.tsx
@@ -53,9 +53,9 @@ export default () => {
       .then(setPhotos)
   }, [mediaType])
 
-  const columnCount = Math.floor(width / 200)
+  const columnCount = Math.floor(width / 202)
   const rowCount = Math.ceil(photos.length / columnCount)
-  const overscanRowCount = Math.round((height / 200) * 2)
+  const overscanRowCount = Math.round((height / 202) * 2)
 
   if (!match) return null
 
@@ -64,10 +64,10 @@ export default () => {
       <Grid
         itemData={{ columnCount, photos }}
         columnCount={columnCount}
-        columnWidth={200}
+        columnWidth={202}
         height={height}
         rowCount={rowCount}
-        rowHeight={200}
+        rowHeight={202}
         width={width}
         overscanRowCount={overscanRowCount}
         innerRef={innerRef}
@@ -232,42 +232,56 @@ const Cell = ({ columnIndex, rowIndex, data, style }: CellProps) => {
   return (
     <div style={{ ...style, top: parseInt((style.top as string) || '0', 10) + NAV_BAR_HEIGHT + 2 }}>
       <Link to={`?index=${index}`}>
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-          {!loaded && <Spinner />}
-          {thumbnail && (
-            <img
-              src={thumbnail}
-              onLoad={() => setLoaded(true)}
-              style={{ position: 'absolute', top: 1, left: 1, width: 198, height: 198 }}
-            />
-          )}
+        {!loaded && (
           <div
             style={{
               position: 'absolute',
-              bottom: 1,
-              right: 1,
-              left: '50%',
-              textAlign: 'right',
-              color: '#adadad',
-              background: 'rgba(0,0,0,0.6)',
-              fontWeight: 'bold',
-              fontSize: 12,
-              paddingTop: 2,
-              paddingBottom: 2,
-              paddingRight: 4,
-              paddingLeft: 4,
+              top: 1,
+              left: 1,
+              width: 200,
+              height: 200,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              background: 'rgb(15 17 21 / 80%)',
             }}
           >
-            {photo.mediaType === 'video' ? (
-              <div style={{ position: 'absolute', left: 4, top: 3 }}>
-                <svg width="12" height="12" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="10" cy="10" r="9" stroke="#E3E3E3" strokeWidth="2" />
-                  <path d="M15 10L7.5 14.3301L7.5 5.66987L15 10Z" fill="#E3E3E3" />
-                </svg>
-              </div>
-            ) : null}
-            {new Date(photo.metadata.createdAt).toLocaleDateString()}
+            <Spinner />
           </div>
+        )}
+        {thumbnail && (
+          <img
+            src={thumbnail}
+            onLoad={() => setLoaded(true)}
+            style={{ position: 'absolute', top: 1, left: 1, width: 200, height: 200 }}
+          />
+        )}
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 1,
+            right: 1,
+            left: '50%',
+            textAlign: 'right',
+            color: '#adadad',
+            background: 'rgba(0,0,0,0.6)',
+            fontWeight: 'bold',
+            fontSize: 12,
+            paddingTop: 2,
+            paddingBottom: 2,
+            paddingRight: 4,
+            paddingLeft: 4,
+          }}
+        >
+          {photo.mediaType === 'video' ? (
+            <div style={{ position: 'absolute', left: 4, top: 3 }}>
+              <svg width="12" height="12" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="10" cy="10" r="9" stroke="#E3E3E3" strokeWidth="2" />
+                <path d="M15 10L7.5 14.3301L7.5 5.66987L15 10Z" fill="#E3E3E3" />
+              </svg>
+            </div>
+          ) : null}
+          {new Date(photo.metadata.createdAt).toLocaleDateString()}
         </div>
       </Link>
     </div>
