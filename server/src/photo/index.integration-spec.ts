cfd1a922b36edcae19328c1a46175866051586b0
Paulo Ragonha <paulo@ragonha.me>
2021-03-08 19:48:00 +0100

    Upgrade exiftool and fix integration tests 🥰

--- server/src/photo/index.integration-spec.ts
+++ server/src/photo/index.integration-spec.ts
@@ -1,20 +1,39 @@
+/**
+ * Stub id generation to get predictable results
+ */
+jest.mock('./generateId', () => {
+  return (fullPath: string) => crypto.createHash('md4').update(fullPath).digest('hex').slice(0, 4)
+})
+
 import { join } from 'path'
 import { readdirSync } from 'fs'
 import rimraf from 'rimraf'
+import crypto from 'crypto'
 
-import loadPhoto from '.'
+import { importPhoto } from '.'
+import { exiftool } from 'exiftool-vendored'
 
 const libraryBasePath = join(__dirname, '../../fixtures')
 const cacheBasePath = join(__dirname, '../../tmp')
 const config = { libraryBasePath, cacheBasePath, httpPort: 80 }
 
+/**
+ * Cleanup any cache we have stored
+ */
 beforeAll(() => rimraf.sync(cacheBasePath))
 
 readdirSync(libraryBasePath)
   .filter((photoPath) => photoPath.match(/\.(arw|jpg|jpeg|mp4|avi|mov|mpg)$/i))
   .forEach((photoPath: string) => {
     it(`loads ${photoPath}`, async () => {
-      const photo = await loadPhoto(config)(photoPath)
+      const photo = await importPhoto(config, photoPath)
       expect(photo).toMatchSnapshot()
     }, 180000)
   })
+
+/**
+ * Stop the singleton instance of the exiftool, otherwise jest is unable to finish
+ */
+afterAll(() => {
+  exiftool.end()
+})
